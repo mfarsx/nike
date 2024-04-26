61f49465cd7decdf4147db31e452682a6a376c47
mfarsx <arslantrkfatih@gmail.com>
2024-04-27 08:41:18 +1000

    Refactor Nav import in App.jsx and add index.js to export components

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ import {
   SpecialOffer,
 } from "./sections";
 
-import Nav from "./components/Nav";
+import { Nav } from "./components";
 
 const App = () => (
   <main className="relative">
     <Nav />
-    <section className="xl:padding-1 wide:padding-r padding-b">
+    <section className="xl:padding-l wide:padding-r padding-b">
       <Hero />
     </section>
     <section className="padding">
